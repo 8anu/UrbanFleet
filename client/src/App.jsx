abff5c5bc96df9667aa3f48ba551912071ae4736
Hemant Vyas <vyashmt@gmail.com>
2025-09-14 03:44:41 +0530

    added the newsletter and some other front-end functionalities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
+import Footer from './components/footer'
 
 const App = () => {
 
@@ -23,6 +24,9 @@ const App = () => {
     <Route path='/my-bookings' element={<MyBookings/>} />
 
     </Routes>
+
+    {!isOwnerPath && <Footer />}
+    
 
     </>
   )
